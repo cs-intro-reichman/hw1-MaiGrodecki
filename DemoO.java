1dac123fe83bda6c35480fe20b614142ffab4b4c
Mai Grodecki <mai.grodecki@post.runi.ac.il>
2025-10-29 23:07:36 +0200

    tasks 1 - 4

---
+++ DemoO.java
@@ -0,0 +1,10 @@
+public class DemoO {
+	public static void main(String[] args) {
+        int i = 0;
+        while(i < 6){
+            System.out.println(i);
+            i = i + 1;
+        }
+        System.out.println("Done");
+    }
+ }
